99c845c043daec832edb86d5f769e2910bc86672
Yukpay.id <113568418+junioryukpay@users.noreply.github.com>
2026-08-06 01:33:40 +0800

    Update Tampilan

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,12 @@
 export default defineAppConfig({
-  title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: 'Shortlink',
+  logo: 'https://i.ibb.co.com/Jwq9G740/sdku.png',
+  github: '',
+  coffee: '',
+  twitter: '',
+  telegram: '',
+  description: 'Platform Pemendek Tautan & Kode QR Internal SD No. 2 Mengwi.',
+  image: 'https://i.ibb.co.com/Jwq9G740/sdku.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
